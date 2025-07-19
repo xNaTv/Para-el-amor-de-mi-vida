2d73c7b695bebb82bf84fffa7253724ac79aac4c
xNaTv <nicolastoloza2001@gmail.com>
2025-07-19 14:00:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ document.querySelectorAll(".box button").forEach(button => {
 
     const userCode = prompt("Ingresá el código secreto:").toLowerCase();
 
-    // Sonido de éxito
-    let audio = new Audio('https://www.myinstants.com/media/sounds/success-fanfare-trumpets.mp3');
-    audio.play();
-
     if (userCode === correctCode) {
+      let audio = new Audio('https://www.myinstants.com/media/sounds/success-fanfare-trumpets.mp3');
+      audio.play();
+
       box.innerHTML = `<div class="desbloqueada"><span class="title">🎁 Desbloqueada</span><p>${content}</p></div>`;
     } else {
       alert("Código incorrecto 😢");
